2e15c6fadbabbc744ba6d3d73b09217dc3464775
D. Potikanond <potikanond@gmail.com>
2024-07-24 22:49:53 +0700

    Natdanai

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,12 @@
 import React from "react";
+ interface Namep {
+  year: string;
+  fullName:string;
+  studentId: string;
 
-export default function Footer(props) {
+}
+export default function Footer(props: Namep) {
+
   return (
     <div className="mt-3">
       <p className="text-secondary text-center">
